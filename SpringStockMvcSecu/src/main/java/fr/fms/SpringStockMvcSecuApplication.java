eed03be2f8f4d46f75180539123631d2f764db6c
Saralune <sarahlef59@hotmail.fr>
2022-06-14 11:22:49 +0200

    init Spring Project

---
+++ SpringStockMvcSecu/src/main/java/fr/fms/SpringStockMvcSecuApplication.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockMvcSecuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStockMvcSecuApplication.class, args);
+	}
+
+}
